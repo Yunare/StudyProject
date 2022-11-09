e33f5f388d7ecb52cbbd45266353f8bc6118596a
Yunare <yu.na.re.work@gmail.com>
2022-11-09 21:26:22 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const answers = [];
+
+answers[0] = prompt('Як ваше імя?', 'Ваше імя')
+answers[1] = prompt('Скільки вам років?', '')
+answers[2] = prompt('Який улюблений фільм', '')
+
+console.log(answers)
