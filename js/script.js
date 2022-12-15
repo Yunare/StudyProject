c23281a8a9d22b404c66b455b8f058a40a48ec8f
Yunare <yu.na.re.work@gmail.com>
2022-12-15 19:20:20 +0200

    добавлення автоматики питання

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const numberOfFilms = +prompt('Скільки фільмов ви вже подививилися', 'Впишить кількість фільмов');
 
-
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -9,13 +8,43 @@ const personalMovieDB = {
     privat: false
 };
 
-const nameMovies = prompt('Один з останіх продивляних фільмов?', 'Впишіть назву фільму');
-const ratingMovies = prompt('На скільки ви оцінюєте його?', 'Впишіть оцінку фільма');
+// Автомізуємо питання з допомогою циклів
+for (let i = 0; i < 2; i++) {
 
-const nameMovies2 = prompt('Один з останіх продивляних фільмов?', 'Впишіть назву фільму');
-const ratingMovies2 = prompt('На скільки ви оцінюєте його?', 'Впишіть оцінку фільма');
+    const nameMovies = prompt('Один з останіх продивляних фільмов?', 'Впишіть назву фільму');
+    const ratingMovies = prompt('На скільки ви оцінюєте його?', 'Впишіть оцінку фільма');
 
-personalMovieDB.movies[nameMovies] = ratingMovies;
-personalMovieDB.movies[nameMovies2] = ratingMovies2;
+    /*  Перевіряємо щоб не було пустої строчки,  
+    щоб користувач не нажав на кнопку відміна, щоб довжина строчки не
+    привешала 50 символів (Ми використовуємо .length для того щоб перевірити 
+    скільки символів в відповіді на питання), і якщо користувач ввів
+    все правильно то ми продовжуємо наш алгоритм */
+    if (nameMovies != null && ratingMovies != null && nameMovies != '' && ratingMovies != '' && nameMovies.length < 50 && ratingMovies.length < 50) {
 
-console.log(personalMovieDB); 
+        personalMovieDB.movies[nameMovies] = ratingMovies;
+
+        console.log('done');
+    } else {
+        console.log('error');
+        /* Якщо више условія не виконалось, то ми повинні вернутись 
+        на один шаг цикла назад, тому нам треба зробити і-- (одна ітерація назад)*/
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фільмов');
+
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('ви класичний переглядач');
+
+} else if (personalMovieDB.count >= 30) {
+    console.log('ви кіноман');
+} else {
+    console.log('Виникла проблема');
+
+}
+
+
+
+console.log(personalMovieDB);
